0e0effb00eef9b097af380ab30e5dff49f986c9e
Eric Klinkhammer <eklinkhammer@gmail.com>
2024-01-29 13:35:11 -0800

    Add Data class for Gems, dependencies to .cabal
    This class represents the tokens of varying colors, as well
    as the wild golds. Module has methods to initialize the
    starting token amounts for the game and the players.
    
    Because Gems is a map between tokens and count, it will also be
    used to model price of cards and value of cards (note that in the
    expansion cards can be worth multiple gems).

---
+++ src/Game/Data/Gems.hs
@@ -0,0 +1,38 @@
+module Game.Data.Gems (
+  Color (..),
+  Gold (..),
+  Gems (..),
+  Token (..),
+  startingBank,
+  startingHand
+) where
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+data Color = Red | White | Black | Green | Blue
+  deriving (Show, Eq, Enum, Ord)
+
+data Gold = Gold
+  deriving (Show, Eq, Enum, Ord)
+
+data Token = Gem Color | Wild Gold
+  deriving (Show, Eq, Ord)
+
+type Gems = Map Token Int
+
+numberOfWildsInGame = 5
+startingGemsPerPlayer = 0
+startingWildsPerPlayer = 0
+
+startingBank :: Int -> Gems
+startingBank gemCount = initGems gemCount numberOfWildsInGame
+
+startingHand :: Gems
+startingHand = initGems startingGemsPerPlayer startingWildsPerPlayer
+
+initGems :: Int -> Int -> Gems
+initGems gemCount wildCount = Map.fromList (gems ++ wilds)
+   where
+     wilds = [(Wild Gold, wildCount)]
+     gems = zipWith (,) [Gem Red, Gem Blue, Gem Black, Gem Green, Gem White] (repeat gemCount)
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,35 @@
 module Main where
 
+import Game.Data.Gems
+
+import GHC.Generics (Generic)
+import Data.List (groupBy, sort)
+
+data Player = Player {
+  name :: String,
+  gems :: Gems,
+  cards :: [Card],
+  reservedCards :: [Card]
+} deriving (Show, Eq)
+
+data Card = Card {
+  cost :: Gems,
+  points :: Int,
+  value :: Gems
+} deriving (Show, Eq)
+
+data Board = Board {
+  decks :: ([Card], [Card], [Card]),
+  faceup :: ([Card], [Card], [Card]),
+  bank :: Gems
+} deriving (Show, Eq)
+
+data Game = Game {
+  players :: [Player],
+  board :: Board
+} deriving (Show, Eq)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let player = Player "Eric" startingHand [] []
+  putStrLn $ show player
